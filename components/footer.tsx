0f8ef29bab8f84dec59678856c8ca672c04e2d08
Dagim Asnake <dagimasnake388@gmail.com>
2026-02-28 17:01:38 +0300

    finhed the footer comp

--- components/footer.tsx
+++ components/footer.tsx
@@ -8,7 +8,7 @@ export default function (){
         <footer className="flex  items-center pr-2 bg-purple-200 min-w-fit justify-between p-4 text-green-900">
            <div className="ml-6">  &copy;{new Date().getFullYear()}</div>
            <div className="socials flex gap-4 mr-6">
-              <Link href=" https://www.linkedin.com/in/dagim-asnake/"> <FaLinkedin className="h-6 w-6" /></Link>
+              <Link href=" https://www.linkedin.com/in/dagim-asnake/" className=""> <FaLinkedin className="h-6 w-6" /></Link>
               <Link href="https://t.me/dagi_as"> <FaTelegram className="h-6 w-6" /></Link>
               <Link href="https://github.com/dagix7"> <FaGithub className="h-6 w-6" /></Link>
            </div>
