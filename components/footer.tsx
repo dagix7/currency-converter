1f40133a9d8f3efb4212964709f05348f0d62df6
Dagim Asnake <dagimasnake388@gmail.com>
2026-02-28 17:07:30 +0300

    added security on social links of footer component

--- components/footer.tsx
+++ components/footer.tsx
@@ -8,9 +8,9 @@ export default function (){
         <footer className="flex  items-center pr-2 bg-purple-200 min-w-fit justify-between p-4 text-green-900">
            <div className="ml-6">  &copy;{new Date().getFullYear()}</div>
            <div className="socials flex gap-4 mr-6">
-              <Link href=" https://www.linkedin.com/in/dagim-asnake/" className=""> <FaLinkedin className="h-6 w-6" /></Link>
-              <Link href="https://t.me/dagi_as"> <FaTelegram className="h-6 w-6" /></Link>
-              <Link href="https://github.com/dagix7"> <FaGithub className="h-6 w-6" /></Link>
+              <Link href=" https://www.linkedin.com/in/dagim-asnake/" target="_blank" rel="noopener noreferrer"> <FaLinkedin className="h-6 w-6" /></Link>
+              <Link href="https://t.me/dagi_as"target="_blank" rel="noopener noreferrer"> <FaTelegram className="h-6 w-6" /></Link>
+              <Link href="https://github.com/dagix7"target="_blank" rel="noopener noreferrer"> <FaGithub className="h-6 w-6" /></Link>
            </div>
 
         </footer>
