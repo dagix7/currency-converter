34869193dcc011630ad91b14bdba4581d8b881a5
Dagim Asnake <dagimasnake388@gmail.com>
2026-02-28 17:00:48 +0300

    finished working on the footer and also modified the styles in the root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/footer";
 
 export default function RootLayout({
   children,
@@ -9,9 +10,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className="flex flex-col min-h-screen justify-between">
         <Navbar/>
-        {children}
+        <div className="grow">{children}</div>
+        <Footer/>
       </body>
     </html>
   );
---
+++ components/footer.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import { FaLinkedin,FaTelegram,FaGithub } from 'react-icons/fa';
+export default function (){
+
+
+    return(
+
+        <footer className="flex  items-center pr-2 bg-purple-200 min-w-fit justify-between p-4 text-green-900">
+           <div className="ml-6">  &copy;{new Date().getFullYear()}</div>
+           <div className="socials flex gap-4 mr-6">
+              <Link href=" https://www.linkedin.com/in/dagim-asnake/"> <FaLinkedin className="h-6 w-6" /></Link>
+              <Link href="https://t.me/dagi_as"> <FaTelegram className="h-6 w-6" /></Link>
+              <Link href="https://github.com/dagix7"> <FaGithub className="h-6 w-6" /></Link>
+           </div>
+
+        </footer>
+    )
+}
